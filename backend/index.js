6e0db92786ffc2cf45cad0f334a6be90774773a3
SaiReddy0045 <tathireddysaikumarreddy45@gmail.com>
2026-02-10 23:09:33 +0530

    created database

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import app from './app.js';
+import connectDB from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: '../.env'
+});
+
+const startServer = async () => {
+    await connectDB();
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+startServer();
